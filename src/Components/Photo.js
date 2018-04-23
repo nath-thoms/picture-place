06fc3d8eea205cc191cf4266131294fe416211e8
Nath Chadwick <nath.chadwick@gmail.com>
2018-04-23 14:41:11 +0100

    refactor to remove class on stateless functions

--- src/Components/Photo.js
+++ src/Components/Photo.js
@@ -1,18 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PhotoWall from './Photowall';
 
-class Photo extends Component {
-    render() {
-        const post = this.props.post
-        return <figure className="figure">
-            <img className="photo" src={post.imageLink} alt={post.description} />
-            <figcaption> <p> {post.description} </p> </figcaption>
+function Photo(props) {
+    const post = props.post
+    return <figure className="figure">
+        <img className="photo" src={post.imageLink} alt={post.description} />
+        <figcaption> <p> {post.description} </p> </figcaption>
 
-            <div className="button-container">
-                <button className="remove-button"> Remove </button>
-            </div>
-        </figure>
-    }
+        <div className="button-container">
+            <button className="remove-button"> Remove </button>
+        </div>
+    </figure>
 }
+
+
+
 
 export default Photo
