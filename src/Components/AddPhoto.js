cf8875b8c8c9b7c07b922049e19c4e9feb7f844f
Nath Chadwick <nath.chadwick@gmail.com>
2018-04-30 11:20:05 +0100

    added handleSubmit

--- src/Components/AddPhoto.js
+++ src/Components/AddPhoto.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react'
 
 class AddPhoto extends Component {
+
+
+    constructor() {
+        super()
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+    }
+
     render() {
         return (
             <div>
                 <h1> insta-Nath </h1>
                 <div className="form">
-                    <form>
-                        <input type="text" placeholder="Link" />
-                        <input type="text" placeholder="Description" />
+                    <form onSubmit={this.handleSubmit}>
+                        <input type="text" placeholder="Link" name="link" />
+                        <input type="text" placeholder="Description" name="description" />
                         <button> Post </button>
                     </form>
                 </div>
