975a7f92b22cd4c1df7d6754a0c5e8d26359ba25
nath <35030272+nath-thoms@users.noreply.github.com>
2018-06-25 16:48:36 +0100

    dispatching addComment

--- src/Redux/actions.js
+++ src/Redux/actions.js
@@ -44,22 +44,27 @@ export function startLoadingPost() {
                 posts.push(childSnapshot.val())
             })
             dispatch(loadPosts(posts))
-        })
-    }
+        }).catch((error) => {
+            console.log(error)
+    })
 }
 
 export function startRemovingPost(index, id) {
     return (dispatch) => {
         return database.ref(`posts/${id}`).remove().then(() => {
             dispatch(removePost(index))
-        })
-    }
+        }).catch((error) => {
+            console.log(error)
+    })
 }
 
 export function startAddingComment(comment, postId) {
     return (dispatch) => {
-        return database.ref('comment/'+postId)
-    }
+        return database.ref('comment/'+postId).push(comment).then(() => {
+            dispatch(addComment(comment, postId))
+        }).catch((error) => {
+            console.log(error)
+    })
 }
 
 export function loadPosts(posts) {
